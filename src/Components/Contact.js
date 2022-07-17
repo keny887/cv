0dfbdd4ffe5b3a487bec9f16d81a9409df17c51c
Kenneth Yeung <kenneth.sin.yeung@accenture.com>
2022-07-17 15:29:13 +0200

    contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,111 +1,94 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+import emailjs from 'emailjs-com';
 
-class Contact extends Component {
-   render() {
+const Contact = () => {
+  const [statusMessage, setStatusMessage] = useState("Message");
 
-      if (this.props.data) {
-         var message = this.props.data.contactmessage;
-      }
-
-      return (
-         <section id="contact">
-            <div className="row section-head">
-
-               <div className="two columns header-col">
-
-                  <h1><span></span></h1>
-
-               </div>
-
-               <div className="ten columns">
-
-                  <p className="lead">{message}</p>
-
-               </div>
-
-            </div>
-
-            <div className="row">
-               <div className="eight columns">
+  function sendEmail(e) {
+    const form = document.querySelector('#contactForm');
+    const statusMessage = document.querySelector('.status-message');
 
 
-                  <form action="" method="post" id="contactForm" name="contactForm">
-                     <fieldset>
+    e.preventDefault();
+    // console.log(data);
+    generateContactNumber();
+    emailjs.sendForm('service_5r8zjjy', 'template_uxxecit', e.target, 'GWuMTG9nkIhwj4ZSq')
+      .then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+        form.reset();
+        setStatusMessage("Message sent!");
+        statusMessage.className = "status-message success";
+        setTimeout(()=> {
+          statusMessage.className = 'status-message'
+        }, 5000);
+      }, function(error) {
+        console.log('FAILED...', error);
+      });
+  }
 
-                        <div>
-                           <label htmlFor="contactName">Name <span className="required">*</span></label>
-                           <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange} />
-                        </div>
 
-                        <div>
-                           <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-                           <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange} />
-                        </div>
+  const [contactNumber, setContactNumber] = useState("000000");
+  
+  const generateContactNumber = () => {
+    const numStr = "000000" + (Math.random() * 1000000 | 0);
+    setContactNumber(numStr.substring(numStr.length - 6));
+  };
 
-                        <div>
-                           <label htmlFor="contactSubject">Subject</label>
-                           <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange} />
-                        </div>
+  return (
+    <section id="contact">
+      <div className="row section-head">
 
-                        <div>
-                           <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                           <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
-                        </div>
+        <div className="two columns header-col">
 
-                        <div>
-                           <button className="submit">Submit</button>
-                           <span id="image-loader">
-                              <img alt="" src="images/loader.gif" />
-                           </span>
-                        </div>
-                     </fieldset>
-                  </form>
+          <h1><span></span></h1>
 
-                  <div id="message-warning"> Error boy</div>
-                  <div id="message-success">
-                     <i className="fa fa-check"></i>Your message was sent, thank you!<br />
-                  </div>
-               </div>
+        </div>
 
-               {/*
-            <aside className="four columns footer-widgets">
-               <div className="widget widget_contact">
+        <div className="ten columns">
 
-					   <h4>Address and Phone</h4>
-					   <p className="address">
-						   {name}<br />
-						   {street} <br />
-						   {city}, {state} {zip}<br />
-						   <span>{phone}</span>
-					   </p>
-				   </div>
+          <p className="lead">Get in Touch</p>
+          <p className='status-message'>{statusMessage}</p>
 
-               <div className="widget widget_tweets">
-                  <h4 className="widget-title">Latest Tweets</h4>
-                  <ul id="twitter">
-                     <li>
-                        <span>
-                        This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                        Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">2 Days Ago</a></b>
-                     </li>
-                     <li>
-                        <span>
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                        eaque ipsa quae ab illo inventore veritatis et quasi
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">3 Days Ago</a></b>
-                     </li>
-                  </ul>
-		         </div>
-            </aside> */}
-            </div>
-         </section>
-      );
-   }
-}
+
+        </div>
+
+      </div>
+
+
+      <div className="row">
+        <div className="eight columns">
+
+          <form onSubmit={sendEmail} id="contactForm" name="contactForm">
+            <fieldset>
+              <div>
+                <label htmlFor="contactName">Name <span className="required">*</span></label>
+                <input type="text" size="35" name="name" className='form-control' />
+              </div>
+
+              <div>
+                <label htmlFor="contactEmail">Email <span className="required">*</span></label>
+                <input type="email" size="35" name="user_email" className='form-control' />
+              </div>
+
+              <div>
+                <label htmlFor="contactMessage">Message <span className="required">*</span></label>
+                <textarea rows="4" id="contactMessage" name="message" className='form-control' ></textarea>
+                <input type='hidden' name='contact_number' value={contactNumber} />
+
+              </div>
+
+              <div>
+                <input type="submit" name="Send" className=" submit form-control btn btn-primary" value='Send'/>
+                <span id="image-loader">
+                  <img alt="" src="images/loader.gif" />
+                </span>
+              </div>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+}; 
 
 export default Contact;
